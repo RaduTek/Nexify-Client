5c44612009a8ab729b13437bc1ce2aa753ce05cb
RaduTek <radutek13@gmail.com>
2024-04-09 17:31:04 +0300

    Add desktop sidebar to desktop layout

--- src/layouts/Desktop/index.tsx
+++ src/layouts/Desktop/index.tsx
@@ -1,9 +1,16 @@
 import { Outlet } from "react-router-dom";
+import DesktopSidebar from "../../components/DesktopSidebar";
+import { Box, Stack } from "@mui/material";
 
 export default function DesktopLayout() {
   return (
     <>
-      <Outlet />
+      <Stack flexDirection="row">
+        <DesktopSidebar />
+        <Box sx={{ flexGrow: 1 }}>
+          <Outlet />
+        </Box>
+      </Stack>
     </>
   );
 }

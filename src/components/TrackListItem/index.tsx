5a367d91b6c1c8d14e51abd390568f26119f1892
RaduTek <radutek13@gmail.com>
2024-04-09 16:36:15 +0300

    Update track list item

--- src/components/TrackListItem/index.tsx
+++ src/components/TrackListItem/index.tsx
@@ -1,24 +1,38 @@
-import { MoreVert } from "@mui/icons-material";
+import { Audiotrack, MoreVert } from "@mui/icons-material";
 import {
+  Avatar,
+  Divider,
   IconButton,
   ListItem,
+  ListItemAvatar,
   ListItemButton,
   ListItemText,
 } from "@mui/material";
 
-export default function TrackListItem() {
+export default function TrackListItem({ last }: { last?: boolean }) {
   return (
-    <ListItem
-      secondaryAction={
-        <IconButton sx={{ p: 1.5 }}>
-          <MoreVert />
-        </IconButton>
-      }
-      disablePadding
-    >
-      <ListItemButton>
-        <ListItemText primary="Track Name" secondary="Track Artist - 1:23" />
-      </ListItemButton>
-    </ListItem>
+    <>
+      <ListItem
+        secondaryAction={
+          <IconButton sx={{ p: 1.5 }}>
+            <MoreVert />
+          </IconButton>
+        }
+        disablePadding
+      >
+        <ListItemButton>
+          <ListItemAvatar>
+            <Avatar
+              variant="rounded"
+              sx={{ width: "56px", height: "56px", mr: 2 }}
+            >
+              <Audiotrack fontSize="large" />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Track Name" secondary="Track Artist - 1:23" />
+        </ListItemButton>
+      </ListItem>
+      {!last && <Divider />}
+    </>
   );
 }

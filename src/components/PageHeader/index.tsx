4bc9d53344554b14ac619b8d62e73f8f9eec9cee
RaduTek <radutek13@gmail.com>
2024-04-09 17:30:15 +0300

    Add content spacer after the toolbar

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,15 +1,20 @@
 import HeaderProps from "./headerProps";
-import { useMediaQuery, useTheme } from "@mui/material";
+import { Toolbar, useMediaQuery, useTheme } from "@mui/material";
 import MobilePageHeader from "./MobilePageHeader";
 import DesktopPageHeader from "./DesktopPageHeader";
 
 export default function PageHeader(props: HeaderProps) {
   const theme = useTheme();
-  const mobileSize = useMediaQuery(theme.breakpoints.down("md"));
+  const mobileSize = useMediaQuery(theme.breakpoints.down("sm"));
 
-  return mobileSize ? (
-    <MobilePageHeader {...props} />
-  ) : (
-    <DesktopPageHeader {...props} />
+  return (
+    <>
+      {mobileSize ? (
+        <MobilePageHeader {...props} />
+      ) : (
+        <DesktopPageHeader {...props} />
+      )}
+      <Toolbar />
+    </>
   );
 }

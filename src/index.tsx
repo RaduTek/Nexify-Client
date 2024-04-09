cc012e51119dc3fd5a7baf3d734f66001ac27c7c
RaduTek <radutek13@gmail.com>
2024-04-09 12:04:42 +0300

    Route definitions

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import RootLayout from "./layouts/Root";
+import MainLayout from "./layouts/Main";
+import HomePage from "./pages/Home";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <div>Login page</div>,
+  },
+  {
+    path: "/signup",
+    element: <div>Signup page</div>,
+  },
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to="/home" replace />,
+      },
+      {
+        path: "home",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RootLayout>
+      <RouterProvider router={router} />
+    </RootLayout>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

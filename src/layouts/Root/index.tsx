52886ec35817b04031edd3f53946325a8ee7edae
RaduTek <radutek13@gmail.com>
2024-04-09 12:04:01 +0300

    Root layout

---
+++ src/layouts/Root/index.tsx
@@ -0,0 +1,21 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { ReactNode } from "react";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+}
